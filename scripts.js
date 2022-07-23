df419e8d5cd71c9f6660d167cf8050e8f3c06a61
aegis945 <artiom.lekicev@gmail.com>
2022-07-23 23:35:07 +0300

    Rearranged conditions and added FizzBuzz condition

--- scripts.js
+++ scripts.js
@@ -1,12 +1,13 @@
 let answer = parseInt(prompt("Please enter a number"));
 
 for (let i=1; i<=answer; i++) {
-    if (i % 3 === 0){
+    if (i % 3 === 0 && i % 5 === 0){
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
         console.log("Fizz");
     } else if (i % 5 === 0) {
         console.log("Buzz");
-    }
-    else {
-        console.log(i);
+    } else {
+            console.log(i);
     }
 }
